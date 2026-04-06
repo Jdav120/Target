a143d946298d9efb0c9afedfc3a6541d327f0ebe
Davis, Justin <jdav120@okstate.edu>
2026-04-06 11:47:39 -0500

    First Push

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Bind to port 5000 for Elastic Beanstalk (nginx reverse proxies 80 -> 5000)
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +15,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Health check endpoint for Elastic Beanstalk (do not remove)
+app.MapGet("/health", () => Results.Ok(new { status = "healthy" }));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
